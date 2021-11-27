30ddb23301c51c6e1f3fcec0959123d553b4b4e0
Siqi Tao <taosiqi728@163.com>
2021-11-27 11:50:12 -0800

    Copy function from 571 notes and include scoring

--- src/machine learning analysis.py
+++ src/machine learning analysis.py
@@ -45,6 +45,35 @@ from sklearn.preprocessing import (
 from sklearn.svm import SVC, SVR
 from scipy.stats import loguniform
 
+# Function copied from DSCI571 note
+def mean_std_cross_val_scores(model, X_train, y_train, **kwargs):
+    """
+    Returns mean and std of cross validation
+
+    Parameters
+    ----------
+    model :
+        scikit-learn model
+    X_train : numpy array or pandas DataFrame
+        X in the training data
+    y_train :
+        y in the training data
+
+    Returns
+    ----------
+        pandas Series with mean scores from cross_validation
+    """
+
+    scores = cross_validate(model, X_train, y_train, **kwargs)
+
+    mean_scores = pd.DataFrame(scores).mean()
+    std_scores = pd.DataFrame(scores).std()
+    out_col = []
+
+    for i in range(len(mean_scores)):
+        out_col.append((f"%0.3f (+/- %0.3f)" % (mean_scores[i], std_scores[i])))
+
+    return pd.Series(data=out_col, index=mean_scores.index)
 
 opt = docopt(__doc__)
 
@@ -74,7 +103,28 @@ def main():
         OneHotEncoder(handle_unknown="ignore", sparse=False), 
         LogisticRegression(max_iter=2000)
     )
+    
+    # Hyperparameter tuning using RandomSearchCV 
+    param_grid = {
+        "logisticregression__C": loguniform(1e-3, 1e3),
+    }
+    
+    # Scoring include f1, recall, precision. 
+    results = {}
+    scoring = [
+        "f1", 
+        "recall", 
+        "precision", 
+        "roc_auc", 
+        "average_precision"
+    ]
+    results["Logistic Regression"] = mean_std_cross_val_scores(pipe, X_train, y_train, scoring=scoring)
+    result_df = pd.DataFrame(results)
+    
 
+    
+
+    # Feature selection using pipeline including OneHotEncoder, RFECV, LogisticRegression.
     
 if __name__ == "__main__":
     main() 
