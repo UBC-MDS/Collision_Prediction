05c3da4e38a6a514ca1588b02f206e4a217d6474
Daniel King <dnlkng13@gmail.com>
2021-12-03 02:52:55 -0800

    Create new split ratio, update target column, update read_csv

--- src/clean_split_data.py
+++ src/clean_split_data.py
@@ -20,20 +20,20 @@ opt = docopt(__doc__)
 
 def main():
     # Importing NCDB 2017 dataset
-    ncdb = pd.read_csv(opt["--input"], low_memory=False).sort_index()
+    ncdb = pd.read_csv(opt["--input"], low_memory=False, skiprows=1).sort_index()
 
     # Make all columns contain strings
     ncdb = ncdb.astype("string")
 
-    # Creating 'FATALITY' column to convert multi-class outcomes to binary-class
-    ncdb.loc[ncdb["P_ISEV"] == "3", "FATALITY"] = True
-    ncdb.loc[ncdb["P_ISEV"] != "3", "FATALITY"] = False
+    # Create 'FATALITY' column
+    ncdb.loc[ncdb["C_SEV"] == "1", "FATALITY"] = int(1)
+    ncdb.loc[ncdb["C_SEV"] != "1", "FATALITY"] = int(0)
 
     # Dropping irrelevant or redundant columns
-    ncdb = ncdb.drop(columns=["C_YEAR", "C_CASE", "C_SEV", "P_ISEV", "V_ID", "P_ID"])
+    ncdb = ncdb.drop(columns=["C_YEAR", "C_CASE", "P_ISEV", "V_ID", "P_ID", "C_SEV"])
 
-    # Split data into train and test split (90:10)
-    train_df, test_df = train_test_split(ncdb, test_size=0.1, random_state=21)
+    # Split data into train and test split (80:20)
+    train_df, test_df = train_test_split(ncdb, test_size=0.2, random_state=21)
 
     # Set unknown, data not provided by jurisdiction, and "other" values to missing
     null_value = ["N", "NN", "NNNN", "Q", "QQ", "U", "UU", "UUUU", "X", "XX", "XXXX"]
